8673a0b2f98f2b9d4c99cd7d55c0ceb20f612330
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-08-02 23:14:57 +0200

    update version to 0.2.0

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -22,7 +22,7 @@ publishing {
             }
             groupId = "sergio.sastre.composable.preview.scanner"
             artifactId = "jvm"
-            version = "0.1.3"
+            version = "0.2.0"
         }
     }
 }
