58da16d08db80975bc12eca224744dfabfc86afa
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-09-29 11:09:54 +0200

    working changes to publish to maven central

--- glance/build.gradle.kts
+++ glance/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jetbrains.kotlin.plugin.compose)
+    id("com.vanniktech.maven.publish") version "0.29.0"
+    id("com.gradleup.nmcp") version "0.0.8"
     id("maven-publish")
 }
 
@@ -35,12 +39,15 @@ android {
         compose = true
     }
 
+    /*
     publishing {
         singleVariant("release") {
             withSourcesJar()
             withJavadocJar()
         }
     }
+    
+     */
 }
 
 dependencies {
@@ -59,7 +66,40 @@ dependencies {
     implementation(libs.kotlin.reflect)
 }
 
+//https://medium.com/@iRYO400/how-to-upload-your-android-library-to-maven-central-central-portal-in-2024-af7348742247
+mavenPublishing {
+    coordinates("io.github.sergio-sastre.ComposablePreviewScanner", "glance", "0.7.0")
+    pom {
+        name = "ComposablePreviewScanner"
+        description = "A library to help auto-generate screenshot tests from Composable Previews with any screenshot testing library"
+        inceptionYear = "2024"
+        url = "https://github.com/sergio-sastre/ComposablePreviewScanner"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/license/mit"
+                distribution = "https://github.com/sergio-sastre/ComposablePreviewScanner/blob/master/LICENSE"
+            }
+        }
+        developers {
+            developer {
+                id = "sergio-sastre"
+                name = "Sergio Sastre Flórez"
+                url = "https://github.com/sergio-sastre/ComposablePreviewScanner"
+            }
+        }
+        scm {
+            url = "https://github.com/sergio-sastre/ComposablePreviewScanner"
+            connection = "scm:git:git://github.com/sergio-sastre/ComposablePreviewScanner.git"
+            developerConnection = "scm:git:ssh://git@github.com/sergio-sastre/ComposablePreviewScanner.git"
+        }
+    }
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+}
+
 //https://www.talentica.com/blogs/publish-your-android-library-on-jitpack-for-better-reachability/
+/*
 publishing {
     publications {
         create<MavenPublication>("release") {
@@ -72,3 +112,5 @@ publishing {
         }
     }
 }
+
+ */
