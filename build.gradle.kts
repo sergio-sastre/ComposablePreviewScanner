8fd1e88635759d331d10ca500371650e7c6a40b7
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2025-07-30 22:07:53 +0200

    remove unnecessary plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.plugin.compose) apply false
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.paparazzi) apply false
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-    alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.jetbrains.kotlin.plugin.compose)
     id("maven-publish")
 }
