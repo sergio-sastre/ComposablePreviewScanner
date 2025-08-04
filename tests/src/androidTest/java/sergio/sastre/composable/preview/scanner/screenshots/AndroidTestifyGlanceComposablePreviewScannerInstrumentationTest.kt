9d30c705b3f9c542b507c034f566336b2d9c174a
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2025-08-04 20:05:27 +0200

    Add Instrumentation tests for Glance with Android-Testify

---
+++ tests/src/androidTest/java/sergio/sastre/composable/preview/scanner/screenshots/AndroidTestifyGlanceComposablePreviewScannerInstrumentationTest.kt
@@ -0,0 +1,97 @@
+package sergio.sastre.composable.preview.scanner.screenshots
+
+import android.content.res.AssetManager
+import android.view.ViewGroup
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import dev.testify.annotation.ScreenshotInstrumentation
+import dev.testify.core.processor.capture.pixelCopyCapture
+import dev.testify.scenario.ScreenshotScenarioRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import sergio.sastre.composable.preview.scanner.MainActivity
+import sergio.sastre.composable.preview.scanner.core.preview.ComposablePreview
+import sergio.sastre.composable.preview.scanner.core.scanner.config.classpath.Classpath
+import sergio.sastre.composable.preview.scanner.core.scanner.config.classpath.SourceSet.MAIN
+import sergio.sastre.composable.preview.scanner.glance.GlanceComposablePreviewScanner
+import sergio.sastre.composable.preview.scanner.glance.GlancePreviewInfo
+import sergio.sastre.composable.preview.scanner.glance.GlanceWrapper
+import sergio.sastre.uitesting.android_testify.screenshotscenario.assertSame
+import sergio.sastre.uitesting.android_testify.screenshotscenario.generateDiffs
+import sergio.sastre.uitesting.utils.activityscenario.activityScenarioForActivityRule
+import sergio.sastre.uitesting.utils.utils.waitForActivity
+
+/**
+ * ./gradlew :tests:screenshotRecord -PincludeSourceSetScreenshotTest
+ *
+ * if executed without gradle property -PincludeSourceSetScreenshotTest,
+ * it should throw ClassInScreenshotTestSourceSetNotFoundException
+ */
+@RunWith(Parameterized::class)
+class TestifyGlanceComposablePreviewScannerInstrumentationInvokeTest(
+    private val preview: ComposablePreview<GlancePreviewInfo>,
+) {
+
+    companion object {
+        private val assets: AssetManager by lazy {
+            getInstrumentation().context.assets
+        }
+
+        private val cachedMainPreviews: List<ComposablePreview<GlancePreviewInfo>> by lazy {
+            GlanceComposablePreviewScanner()
+                .setTargetSourceSet(Classpath(MAIN))
+                .scanFile(
+                    targetInputStream = assets.open("scan_result_main.json"),
+                    customPreviewsInfoInputStream = assets.open("custom_previews.json")
+                )
+                .includePrivatePreviews()
+                .getPreviews()
+                .apply {
+                    println("Preview count: ${this.size}")
+                }
+        }
+
+        @JvmStatic
+        @Parameterized.Parameters
+        fun values(): List<ComposablePreview<GlancePreviewInfo>> = cachedMainPreviews
+    }
+
+    @get:Rule
+    val screenshotRule = ScreenshotScenarioRule()
+
+    @get:Rule
+    val composableRule = activityScenarioForActivityRule<MainActivity>()
+
+    @ScreenshotInstrumentation
+    @Test
+    fun snapshot() {
+        screenshotRule
+            .withScenario(composableRule.activityScenario)
+            .setScreenshotViewProvider {
+                composableRule
+                    .activityScenario
+                    .onActivity {
+                        val view =
+                            GlanceWrapper(it.window.decorView as ViewGroup)
+                                .setSizeDp(
+                                    widthDp = preview.previewInfo.widthDp,
+                                    heightDp = preview.previewInfo.heightDp
+                                )
+                                .renderComposable { preview() }
+                        (view.parent as ViewGroup).removeView(view)
+                        it.setContentView(view)
+                    }
+                    .waitForActivity()
+                    .window
+                    .decorView
+                    .findViewById<ViewGroup>(android.R.id.content)
+                    .getChildAt(0)
+            }
+            .configure { captureMethod = ::pixelCopyCapture }
+            .generateDiffs(true)
+            .assertSame(
+                name = "${preview.previewInfo.widthDp}_${preview.previewInfo.heightDp}"
+            )
+    }
+}
