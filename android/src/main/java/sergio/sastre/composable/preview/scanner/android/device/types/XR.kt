b86cce3fa93c00ce166c0b06d01386ebc90c60aa
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2025-09-08 22:21:58 +0200

    Refactor updated devices to avoid duplicated code

--- android/src/main/java/sergio/sastre/composable/preview/scanner/android/device/types/XR.kt
+++ android/src/main/java/sergio/sastre/composable/preview/scanner/android/device/types/XR.kt
@@ -5,11 +5,23 @@ import sergio.sastre.composable.preview.scanner.android.device.domain.Dimensions
 import sergio.sastre.composable.preview.scanner.android.device.domain.GetDeviceByIdentifier
 import sergio.sastre.composable.preview.scanner.android.device.domain.Identifier
 import sergio.sastre.composable.preview.scanner.android.device.domain.Orientation.LANDSCAPE
-import sergio.sastre.composable.preview.scanner.android.device.domain.Orientation.PORTRAIT
 import sergio.sastre.composable.preview.scanner.android.device.domain.Shape.NOTROUND
-import sergio.sastre.composable.preview.scanner.android.device.domain.Type
 import sergio.sastre.composable.preview.scanner.android.device.domain.Type.*
 import sergio.sastre.composable.preview.scanner.android.device.domain.Unit.PX
+
+private val XR_DEFAULT = Device(
+    identifier = null,
+    dimensions = Dimensions(
+        width = 2560f,
+        height = 2558f,
+        unit = PX
+    ),
+    densityDpi = 320,
+    orientation = LANDSCAPE,
+    shape = NOTROUND,
+    chinSize = 0,
+    type = XR
+)
 
 enum class XR(
     override val device: Device
@@ -18,34 +30,10 @@ enum class XR(
     // The Id was changed in more recent versions. New one reflects XR_HEADSET
     // This is kept for compatibility with older versions of Android studio
     XR_DEVICE(
-        Device(
-            identifier = Identifier.XR_DEVICE,
-            dimensions = Dimensions(
-                width = 2560f,
-                height = 2558f,
-                unit = PX
-            ),
-            densityDpi = 320,
-            orientation = LANDSCAPE,
-            shape = NOTROUND,
-            chinSize = 0,
-            type = XR
-        )
+        XR_DEFAULT.copy(identifier = Identifier.XR_DEVICE)
     ),
 
     XR_HEADSET(
-        Device(
-            identifier = Identifier.XR_HEADSET,
-            dimensions = Dimensions(
-                width = 2560f,
-                height = 2558f,
-                unit = PX
-            ),
-            densityDpi = 320,
-            orientation = LANDSCAPE,
-            shape = NOTROUND,
-            chinSize = 0,
-            type = XR
-        )
-    )
+        XR_DEFAULT.copy(identifier = Identifier.XR_HEADSET)
+    );
 }
