bab5e0fee44ac20860a448e6778e1728df25f163
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2026-01-17 15:59:23 +0100

    Refactor: Extract static helper classes
    Extracted static helper classes (Dimensions, ScreenDimensions, DeviceConfigBuilder, SnapshotHandlers, PaparazziPreviewRule, ComposableHelpers) into separate methods in PaparazziTestGenerator. This significantly reduces the size of the main generation method.

--- paparazzi-plugin/src/main/kotlin/io/github/sergio/sastre/composable/preview/scanner/paparazzi/plugin/PaparazziTestGenerator.kt
+++ paparazzi-plugin/src/main/kotlin/io/github/sergio/sastre/composable/preview/scanner/paparazzi/plugin/PaparazziTestGenerator.kt
@@ -10,200 +10,22 @@ class PaparazziTestGenerator {
         return """
             ${generateImports(packageName)}
             
-            class Dimensions(
-                val screenWidthInPx: Int,
-                val screenHeightInPx: Int
-            )
+            ${generateDimensionsClass()}
 
-            object ScreenDimensions {
-                fun dimensions(
-                    parsedDevice: Device,
-                    widthDp: Int,
-                    heightDp: Int
-                ): Dimensions {
-                    val conversionFactor = parsedDevice.densityDpi / 160f
-                    val previewWidthInPx = ceil(widthDp * conversionFactor).toInt()
-                    val previewHeightInPx = ceil(heightDp * conversionFactor).toInt()
-                    return Dimensions(
-                        screenHeightInPx = when (heightDp > 0) {
-                            true -> previewHeightInPx
-                            false -> parsedDevice.dimensions.height.toInt()
-                        },
-                        screenWidthInPx = when (widthDp > 0) {
-                            true -> previewWidthInPx
-                            false -> parsedDevice.dimensions.width.toInt()
-                        }
-                    )
-                }
-            }
+            ${generateScreenDimensionsObject()}
 
-            object DeviceConfigBuilder {
-                fun build(preview: AndroidPreviewInfo): DeviceConfig {
-                    val parsedDevice =
-                        DevicePreviewInfoParser.parse(preview.device)?.inPx() ?: return DeviceConfig()
-
-                    val dimensions = ScreenDimensions.dimensions(
-                        parsedDevice = parsedDevice,
-                        widthDp = preview.widthDp,
-                        heightDp = preview.heightDp
-                    )
-
-                    return DeviceConfig(
-                        screenHeight = dimensions.screenHeightInPx,
-                        screenWidth = dimensions.screenWidthInPx,
-                        density = Density(parsedDevice.densityDpi),
-                        xdpi = parsedDevice.densityDpi, // not 100% precise
-                        ydpi = parsedDevice.densityDpi, // not 100% precise
-                        size = ScreenSize.valueOf(parsedDevice.screenSize.name),
-                        ratio = ScreenRatio.valueOf(parsedDevice.screenRatio.name),
-                        screenRound = ScreenRound.valueOf(parsedDevice.shape.name),
-                        orientation = ScreenOrientation.valueOf(parsedDevice.orientation.name),
-                        locale = preview.locale.ifBlank { "en" },
-                        fontScale = preview.fontScale,
-                        nightMode = when (preview.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES) {
-                            true -> NightMode.NIGHT
-                            false -> NightMode.NOTNIGHT
-                        }
-                    )
-                }
-            }
+            ${generateDeviceConfigBuilderObject()}
             
             // In order to have full control over the screenshot file names
             // we need to pass our own SnapshotHandler to the Paparazzi TestRule
             private val paparazziTestName =
                 TestName(packageName = "Paparazzi", className = "Preview", methodName = "Test")
             
-            private class PreviewSnapshotVerifier(
-                maxPercentDifference: Double
-            ): SnapshotHandler {
-                private val snapshotHandler = SnapshotVerifier(
-                    maxPercentDifference = maxPercentDifference
-                )
-                override fun newFrameHandler(
-                    snapshot: Snapshot,
-                    frameCount: Int,
-                    fps: Int
-                ): SnapshotHandler.FrameHandler {
-                    val newSnapshot = Snapshot(
-                        name = snapshot.name,
-                        testName = paparazziTestName,
-                        timestamp = snapshot.timestamp,
-                        tags = snapshot.tags,
-                        file = snapshot.file,
-                    )
-                    return snapshotHandler.newFrameHandler(
-                        snapshot = newSnapshot,
-                        frameCount = frameCount,
-                        fps = fps
-                    )
-                }
+            ${generateSnapshotHandlers()}
 
-                override fun close() {
-                    snapshotHandler.close()
-                }
-            }
+            ${generatePaparazziPreviewRuleObject()}
 
-            private class PreviewHtmlReportWriter: SnapshotHandler {
-                private val snapshotHandler = HtmlReportWriter()
-                override fun newFrameHandler(
-                    snapshot: Snapshot,
-                    frameCount: Int,
-                    fps: Int
-                ): SnapshotHandler.FrameHandler {
-                    val newSnapshot = Snapshot(
-                        name = snapshot.name,
-                        testName = paparazziTestName,
-                        timestamp = snapshot.timestamp,
-                        tags = snapshot.tags,
-                        file = snapshot.file,
-                    )
-                    return snapshotHandler.newFrameHandler(
-                        snapshot = newSnapshot,
-                        frameCount = frameCount,
-                        fps = fps
-                    )
-                }
-
-                override fun close() {
-                    snapshotHandler.close()
-                }
-            }
-
-            object PaparazziPreviewRule {
-                const val UNDEFINED_API_LEVEL = -1
-                const val MAX_API_LEVEL = 36
-                
-                fun createFor(preview: ComposablePreview<AndroidPreviewInfo>): Paparazzi {
-                    val previewInfo = preview.previewInfo
-                    val previewApiLevel = when(previewInfo.apiLevel == UNDEFINED_API_LEVEL) {
-                        true -> MAX_API_LEVEL
-                        false -> previewInfo.apiLevel
-                    }
-                    val tolerance = 0.0
-                    return Paparazzi(
-                        environment = detectEnvironment().copy(compileSdkVersion = previewApiLevel),
-                        deviceConfig = DeviceConfigBuilder.build(preview.previewInfo),
-                        supportsRtl = true,
-                        showSystemUi = previewInfo.showSystemUi,
-                        renderingMode = when {
-                            previewInfo.showSystemUi -> SessionParams.RenderingMode.NORMAL
-                            previewInfo.widthDp > 0 && previewInfo.heightDp > 0 -> SessionParams.RenderingMode.FULL_EXPAND
-                            else -> SessionParams.RenderingMode.SHRINK
-                        },
-                        snapshotHandler = when(System.getProperty("paparazzi.test.verify")?.toBoolean() == true) {
-                            true -> PreviewSnapshotVerifier(tolerance)
-                            false -> PreviewHtmlReportWriter()
-                        },
-                        // maxPercentDifference can be configured here if needed
-                        maxPercentDifference = tolerance
-                    )
-                }
-            }
-
-            /**
-             * A composable function that wraps content inside a Box with a specified size
-             * This is used to simulate what previews render when showSystemUi is true:
-             * - The Preview takes up the entire screen
-             * - The Composable still keeps its original size, 
-             * - Background color of the Device is white, 
-             *   but the @Composable background color is the one defined in the Preview
-             */
-            @Composable
-            fun SystemUiSize(
-                widthInDp: Int,
-                heightInDp: Int,
-                content: @Composable () -> Unit
-            ) {
-                Box(Modifier
-                    .size(
-                        width = widthInDp.dp,
-                        height = heightInDp.dp
-                    )
-                    .background(Color.White)
-                ) {
-                    content()
-                }
-            }
-
-            @Composable
-            fun PreviewBackground(
-                showBackground: Boolean,
-                backgroundColor: Long,
-                content: @Composable () -> Unit
-            ) {
-                when (showBackground) {
-                    false -> content()
-                    true -> {
-                        val color = when (backgroundColor != 0L) {
-                            true -> Color(backgroundColor)
-                            false -> Color.White
-                        }
-                        Box(Modifier.background(color)) {
-                            content()
-                        }
-                    }
-                }
-            }
+            ${generateComposableHelpers()}
             
             @RunWith(Parameterized::class)
             class $className(
@@ -299,6 +121,220 @@ class PaparazziTestGenerator {
             import sergio.sastre.composable.preview.scanner.android.device.types.DEFAULT
             import sergio.sastre.composable.preview.scanner.android.screenshotid.AndroidPreviewScreenshotIdBuilder
             import sergio.sastre.composable.preview.scanner.core.preview.ComposablePreview
+        """.trimIndent()
+    }
+
+    private fun generateDimensionsClass(): String {
+        return """
+            class Dimensions(
+                val screenWidthInPx: Int,
+                val screenHeightInPx: Int
+            )
+        """.trimIndent()
+    }
+
+    private fun generateScreenDimensionsObject(): String {
+        return """
+            object ScreenDimensions {
+                fun dimensions(
+                    parsedDevice: Device,
+                    widthDp: Int,
+                    heightDp: Int
+                ): Dimensions {
+                    val conversionFactor = parsedDevice.densityDpi / 160f
+                    val previewWidthInPx = ceil(widthDp * conversionFactor).toInt()
+                    val previewHeightInPx = ceil(heightDp * conversionFactor).toInt()
+                    return Dimensions(
+                        screenHeightInPx = when (heightDp > 0) {
+                            true -> previewHeightInPx
+                            false -> parsedDevice.dimensions.height.toInt()
+                        },
+                        screenWidthInPx = when (widthDp > 0) {
+                            true -> previewWidthInPx
+                            false -> parsedDevice.dimensions.width.toInt()
+                        }
+                    )
+                }
+            }
+        """.trimIndent()
+    }
+
+    private fun generateDeviceConfigBuilderObject(): String {
+        return """
+            object DeviceConfigBuilder {
+                fun build(preview: AndroidPreviewInfo): DeviceConfig {
+                    val parsedDevice =
+                        DevicePreviewInfoParser.parse(preview.device)?.inPx() ?: return DeviceConfig()
+
+                    val dimensions = ScreenDimensions.dimensions(
+                        parsedDevice = parsedDevice,
+                        widthDp = preview.widthDp,
+                        heightDp = preview.heightDp
+                    )
+
+                    return DeviceConfig(
+                        screenHeight = dimensions.screenHeightInPx,
+                        screenWidth = dimensions.screenWidthInPx,
+                        density = Density(parsedDevice.densityDpi),
+                        xdpi = parsedDevice.densityDpi, // not 100% precise
+                        ydpi = parsedDevice.densityDpi, // not 100% precise
+                        size = ScreenSize.valueOf(parsedDevice.screenSize.name),
+                        ratio = ScreenRatio.valueOf(parsedDevice.screenRatio.name),
+                        screenRound = ScreenRound.valueOf(parsedDevice.shape.name),
+                        orientation = ScreenOrientation.valueOf(parsedDevice.orientation.name),
+                        locale = preview.locale.ifBlank { "en" },
+                        fontScale = preview.fontScale,
+                        nightMode = when (preview.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES) {
+                            true -> NightMode.NIGHT
+                            false -> NightMode.NOTNIGHT
+                        }
+                    )
+                }
+            }
+        """.trimIndent()
+    }
+
+    private fun generateSnapshotHandlers(): String {
+        return """
+            private class PreviewSnapshotVerifier(
+                maxPercentDifference: Double
+            ): SnapshotHandler {
+                private val snapshotHandler = SnapshotVerifier(
+                    maxPercentDifference = maxPercentDifference
+                )
+                override fun newFrameHandler(
+                    snapshot: Snapshot,
+                    frameCount: Int,
+                    fps: Int
+                ): SnapshotHandler.FrameHandler {
+                    val newSnapshot = Snapshot(
+                        name = snapshot.name,
+                        testName = paparazziTestName,
+                        timestamp = snapshot.timestamp,
+                        tags = snapshot.tags,
+                        file = snapshot.file,
+                    )
+                    return snapshotHandler.newFrameHandler(
+                        snapshot = newSnapshot,
+                        frameCount = frameCount,
+                        fps = fps
+                    )
+                }
+
+                override fun close() {
+                    snapshotHandler.close()
+                }
+            }
+
+            private class PreviewHtmlReportWriter: SnapshotHandler {
+                private val snapshotHandler = HtmlReportWriter()
+                override fun newFrameHandler(
+                    snapshot: Snapshot,
+                    frameCount: Int,
+                    fps: Int
+                ): SnapshotHandler.FrameHandler {
+                    val newSnapshot = Snapshot(
+                        name = snapshot.name,
+                        testName = paparazziTestName,
+                        timestamp = snapshot.timestamp,
+                        tags = snapshot.tags,
+                        file = snapshot.file,
+                    )
+                    return snapshotHandler.newFrameHandler(
+                        snapshot = newSnapshot,
+                        frameCount = frameCount,
+                        fps = fps
+                    )
+                }
+
+                override fun close() {
+                    snapshotHandler.close()
+                }
+            }
+        """.trimIndent()
+    }
+
+    private fun generatePaparazziPreviewRuleObject(): String {
+        return """
+            object PaparazziPreviewRule {
+                const val UNDEFINED_API_LEVEL = -1
+                const val MAX_API_LEVEL = 36
+                
+                fun createFor(preview: ComposablePreview<AndroidPreviewInfo>): Paparazzi {
+                    val previewInfo = preview.previewInfo
+                    val previewApiLevel = when(previewInfo.apiLevel == UNDEFINED_API_LEVEL) {
+                        true -> MAX_API_LEVEL
+                        false -> previewInfo.apiLevel
+                    }
+                    val tolerance = 0.0
+                    return Paparazzi(
+                        environment = detectEnvironment().copy(compileSdkVersion = previewApiLevel),
+                        deviceConfig = DeviceConfigBuilder.build(preview.previewInfo),
+                        supportsRtl = true,
+                        showSystemUi = previewInfo.showSystemUi,
+                        renderingMode = when {
+                            previewInfo.showSystemUi -> SessionParams.RenderingMode.NORMAL
+                            previewInfo.widthDp > 0 && previewInfo.heightDp > 0 -> SessionParams.RenderingMode.FULL_EXPAND
+                            else -> SessionParams.RenderingMode.SHRINK
+                        },
+                        snapshotHandler = when(System.getProperty("paparazzi.test.verify")?.toBoolean() == true) {
+                            true -> PreviewSnapshotVerifier(tolerance)
+                            false -> PreviewHtmlReportWriter()
+                        },
+                        // maxPercentDifference can be configured here if needed
+                        maxPercentDifference = tolerance
+                    )
+                }
+            }
+        """.trimIndent()
+    }
+
+    private fun generateComposableHelpers(): String {
+        return """
+            /**
+             * A composable function that wraps content inside a Box with a specified size
+             * This is used to simulate what previews render when showSystemUi is true:
+             * - The Preview takes up the entire screen
+             * - The Composable still keeps its original size, 
+             * - Background color of the Device is white, 
+             *   but the @Composable background color is the one defined in the Preview
+             */
+            @Composable
+            fun SystemUiSize(
+                widthInDp: Int,
+                heightInDp: Int,
+                content: @Composable () -> Unit
+            ) {
+                Box(Modifier
+                    .size(
+                        width = widthInDp.dp,
+                        height = heightInDp.dp
+                    )
+                    .background(Color.White)
+                ) {
+                    content()
+                }
+            }
+
+            @Composable
+            fun PreviewBackground(
+                showBackground: Boolean,
+                backgroundColor: Long,
+                content: @Composable () -> Unit
+            ) {
+                when (showBackground) {
+                    false -> content()
+                    true -> {
+                        val color = when (backgroundColor != 0L) {
+                            true -> Color(backgroundColor)
+                            false -> Color.White
+                        }
+                        Box(Modifier.background(color)) {
+                            content()
+                        }
+                    }
+                }
+            }
         """.trimIndent()
     }
 }
