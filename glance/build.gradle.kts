a052ad22a964b36431a569499fb28b95a36080e5
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2026-07-06 18:11:49 +0200

    Update to 0.9.1 (Jitpack)

--- glance/build.gradle.kts
+++ glance/build.gradle.kts
@@ -60,7 +60,7 @@ publishing {
             }
             groupId = "sergio.sastre.composable.preview.scanner"
             artifactId = "glance"
-            version = "0.9.0"
+            version = "0.9.1"
         }
     }
 }
